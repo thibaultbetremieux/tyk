c389d7d72b5d4a1b7e0b434484e2ab5e1fb009e5
Thibault Betremieux <110521261+thibaultbetremieux@users.noreply.github.com>
2024-10-15 10:07:30 +0200

    Update mw_key_expired_check.go

--- gateway/mw_key_expired_check.go
+++ gateway/mw_key_expired_check.go
@@ -19,6 +19,7 @@ func (k *KeyExpired) Name() string {
 
 // ProcessRequest will run any checks on the request on the way through the system, return an error to have the chain fail
 func (k *KeyExpired) ProcessRequest(w http.ResponseWriter, r *http.Request, _ interface{}) (error, int) {
+	logger := k.Logger()
 	session := ctxGetSession(r)
 	logger.Info(fmt.Sprintf("Session: %+v", session))
 
@@ -26,8 +27,6 @@ func (k *KeyExpired) ProcessRequest(w http.ResponseWriter, r *http.Request, _ in
 		return nil, http.StatusOK
 	}
 
-	logger := k.Logger()
-	session := ctxGetSession(r)
 	logger.Info(fmt.Sprintf("Session (after fail): %+v", session))
 	if session == nil {
 		return errors.New("Session state is missing or unset! Please make sure that auth headers are properly applied"), http.StatusBadRequest
